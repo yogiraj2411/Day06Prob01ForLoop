a9f8fb436ecf4eb42a08ac91ed1c01d9eceed9b0
NavyaSivakoti <lakshminavya104@gmail.com>
2020-07-04 00:52:55 +0530

    2 power n code and uc-1 branch created

---
+++ 2PowerNValue.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+#Input is entered by Command Line
+n=$1
+for ((  i=0; i<=n; i++ ))
+do
+	if [[ i -eq 0 ]]
+	then
+		out=1
+		echo "2 power 0" "is" " 1"
+	else
+		out=$((2*out))
+		echo "2 power" $i "is " $out
+	fi
+
+done
