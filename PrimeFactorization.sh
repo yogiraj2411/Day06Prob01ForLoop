9ea12beb287559e9069b7e666e6a98c20ecd9344
NavyaSivakoti <lakshminavya104@gmail.com>
2020-07-05 22:54:23 +0530

    Updated comments

--- PrimeFactorization.sh
+++ PrimeFactorization.sh
@@ -9,10 +9,13 @@ for (( i=2; i<=num; i++ ))
 do
 	while [[ num%i -eq 0 ]]
 	do
+		#find whether divided by i initially its 2 
 		if [[ num%i -eq 0 ]]
 		then
 		echo "$i"
 		fi
+		#if its divisible by i or 2 then divide it then for left over number we can find factors
 		num=$(($num/$i))
 	done
 done
+#loop continues until the last divisible number stored in num and which will be nothing but i
